1361b9c6a8ee7e6479d59d096e38b1bcbe3f3702
Eva Rabasco <40592498+erabasco@users.noreply.github.com>
2018-12-30 19:22:58 +0100

    Update main.js

--- coches/main.js
+++ coches/main.js
@@ -2,7 +2,7 @@
  1. Las propiedades serán Matrícula, Marca, modelo y km y un método que permita visualizar todas sus propiedades.
  2.Pedir por teclado los datos de los coches, F terminará la entrada de datos. Kms sólo podrá ser numérico.
  3.Mostrar todoss los coches ordenados por kms de mayor a menor.
- 4. Mostrar el coche con menor número de kms.
+
  */
 
 "use strict";
